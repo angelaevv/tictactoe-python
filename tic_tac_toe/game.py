5d7043023e3eec7ee75ae6e86cf8a07c364b1cac
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2015-09-25 11:04:12 -0500

    Update game.py

--- tic_tac_toe/game.py
+++ tic_tac_toe/game.py
@@ -1,17 +1,4 @@
-# Copyright 2012 Chris Kline
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+#Este archivo fue ediado para comprobar las características colaboratibas de GitHub - Álvaro Sánchez Díaz
 """
 This module contains the class Game and some exceptions for game play.
 The main methods for Game are make_move and check_winner needed for
